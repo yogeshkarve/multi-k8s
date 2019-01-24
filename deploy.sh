7dbb04f475ea5ae37cc156cba88d70e5555de3b3
yogeshkarve <yogesh.p.karve@gmail.com>
2019-01-24 22:46:57 +0100

    for final deplpyement on google cloud

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t yogeshkarve/multi-client:latest -t yogeshkarve/multi-client:$SHA -f ./client/Dockerfile . /client
+docker build -t yogeshkarve/multi-server:latest -t yogeshkarve/multi-server:$SHA -f ./server/Dockerfile . /server
+docker build -t yogeshkarve/multi-worker:latest -t yogeshkarve/multi-worker:$SHA -f ./worker/Dockerfile . /worker
+docker push yogeshkarve/multi-client:latest
+docker push yogeshkarve/multi-server:latest
+docker push yogeshkarve/multi-worker:latest
+docker push yogeshkarve/multi-client:$SHA
+docker push yogeshkarve/multi-server:$SHA
+docker push yogeshkarve/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=yogeshkarve/multi-server:$SHA
+kubectl set image deployments/client-deployment client=yogeshkarve/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=yogeshkarve/multi-worker:$SHA
